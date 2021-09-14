124fe222a8e63d4a63bd7702d8497376a8976563
Marcos Paulo <mpcc.show@gmail.com>
2021-09-13 21:32:04 -0300

    First commit

---
+++ src/transactions/transactions.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Transaction } from './entities/transaction.entity';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { InjectModel } from '@nestjs/sequelize';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    @InjectModel(Transaction) private transactionModel: typeof Transaction,
+  ) {}
+  create(createTransactionDto: CreateTransactionDto) {
+    return this.transactionModel.create(createTransactionDto);
+  }
+
+  findAll() {
+    return this.transactionModel.findAll();
+  }
+}
